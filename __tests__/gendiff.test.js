244acf0e88ad518056b01ac961c24318f4b0deab
egorlitin <egorulitin@gmail.com>
2021-11-21 14:35:00 +0300

    fix after linter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,7 @@
 import * as fs from 'fs';
 import { test, expect } from '@jest/globals';
-import gendiff from '../bin/gendiff';
 
 const json1 = fs.readFileSync('./file1.json', 'utf-8');
-const json2 = fs.readFileSync('./file2.json', 'utf-8');
 
 test('check type of file', () => {
   expect(typeof json1).toEqual('string');
