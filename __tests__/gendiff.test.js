91a74477f6044a62d81e48114d21929ea0d8a269
egorlitin <egorulitin@gmail.com>
2021-11-21 13:30:10 +0300

    add first test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,10 @@
-// import { test, expect } from '@jest/globals';
-// import gendiff from '../bin/gendiff';
-// import * as fs from 'fs';
+import * as fs from 'fs';
+import { test, expect } from '@jest/globals';
+import gendiff from '../bin/gendiff';
 
-// const json1 = fs.readFileSync(`${__dirname}`, 'utf-8');
+const json1 = fs.readFileSync('./file1.json', 'utf-8');
+const json2 = fs.readFileSync('./file2.json', 'utf-8');
 
-// test('gendiff', () => {
-//   expect(gendiff())
-// });
+test('check type of file', () => {
+  expect(typeof json1).toEqual('string');
+});
