c3cc477af2fdac5dc1b8add430757e260842e143
egorlitin <egorulitin@gmail.com>
2021-11-21 14:56:39 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,15 @@
 import * as fs from 'fs';
 import { test, expect } from '@jest/globals';
+import gendiff from '../bin/gendiff';
 
 const json1 = fs.readFileSync('./file1.json', 'utf-8');
+const json2 = fs.readFileSync('./file2.json', 'utf-8');
 
 test('check type of file', () => {
   expect(typeof json1).toEqual('string');
+});
+
+test('check type of return', () => {
+  const res = gendiff(json1, json2);
+  expect(typeof res).toEqual('string');
 });
