0c2858bb5643b25fb12e32044b648d04b3c284b0
egorlitin <egorulitin@gmail.com>
2021-11-29 23:18:36 +0300

    fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,23 +1,23 @@
 import _ from 'lodash';
 import types from '../helpers/types.js';
 
-const TAB = '    ';
+const TAB = '  ';
 const TAB_LEVEL = 1;
 
 function showValue(value, level) {
-  console.log(value);
+  console.log(value, level);
   //return `${_.isObject(value) ? `{\n${TAB.repeat(level + 1)}  ${children.map(([k, v]) => `${k}: ${v}`)}\n${TAB.repeat(level)}  }` : value}`;
   const arr = _.isObject(value) && Object.entries(value);
-  return `${_.isObject(value) ? `{  ${arr.map(([k, v]) => `\n${TAB.repeat(level + 1)}${k}: ${showValue(v, level + 1)}`)}\n${TAB.repeat(level)}  }` : `${value}`}`;
+  return `${_.isObject(value) ? `{${arr.map(([k, v]) => `\n${TAB.repeat(level + 4)}${k}: ${showValue(v, level + 1)}`)}\n${TAB.repeat(level)}  }` : `${value}`}`;
 }
 
 const dictionary = {
-  [types.ADDED]: ({ key, newValue, children }, level) => `${TAB.repeat(level)}+ ${key}: ${showValue(newValue, level, children)}`,
-  [types.REMOVED]: ({ key, oldValue, children }, level) => `${TAB.repeat(level)}- ${key}: ${showValue(oldValue, level, children)}`,
-  [types.UNCHANGED]: ({ key, oldValue, children }, level) => `${TAB.repeat(level)}  ${key}: ${oldValue}`,
-  [types.CHANGED]: ({ key, oldValue, newValue, children }, level) => `${TAB.repeat(level)}- ${key}: ${showValue(oldValue, level, children)}\n${TAB.repeat(level)}+ ${key}: ${showValue(newValue, level, children)}`,
+  [types.ADDED]: ({ key, newValue, children }, level) => `${TAB.repeat(level)}  + ${key}: ${showValue(newValue, level, children)}`,
+  [types.REMOVED]: ({ key, oldValue, children }, level) => `${TAB.repeat(level)}  - ${key}: ${showValue(oldValue, level, children)}`,
+  [types.UNCHANGED]: ({ key, oldValue, children }, level) => `${TAB.repeat(level)}    ${key}: ${oldValue}`,
+  [types.CHANGED]: ({ key, oldValue, newValue, children }, level) => `${TAB.repeat(level)}  - ${key}: ${showValue(oldValue, level, children)}\n${TAB.repeat(level)}  + ${key}: ${showValue(newValue, level, children)}`,
   // eslint-disable-next-line no-use-before-define
-  [types.NODE]: (node, level) => `${TAB.repeat(level)}${node.key}: ${diff(node.children, level + 1)}`,
+  [types.NODE]: (node, level) => `${TAB.repeat(level)}  ${node.key}: ${diff(node.children, level + 1)}`,
 };
 
 function diff(data, level) {
