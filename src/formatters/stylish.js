6a7d2e9f3916af3ffa07704e83b8df9f2a3da89d
egorlitin <egorulitin@gmail.com>
2021-11-29 22:15:45 +0300

    fix gendiff function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,15 +1,23 @@
+import _ from 'lodash';
 import types from '../helpers/types.js';
 
 const TAB = '    ';
 const TAB_LEVEL = 1;
 
+function showValue(value, level) {
+  console.log(value);
+  //return `${_.isObject(value) ? `{\n${TAB.repeat(level + 1)}  ${children.map(([k, v]) => `${k}: ${v}`)}\n${TAB.repeat(level)}  }` : value}`;
+  const arr = _.isObject(value) && Object.entries(value);
+  return `${_.isObject(value) ? `{  ${arr.map(([k, v]) => `\n${TAB.repeat(level + 1)}${k}: ${showValue(v, level + 1)}`)}\n${TAB.repeat(level)}  }` : `${value}`}`;
+}
+
 const dictionary = {
-  [types.ADDED]: (node, level) => `${TAB.repeat(level)}+ ${node.key}: ${node.newValue}`,
-  [types.REMOVED]: (node, level) => `${TAB.repeat(level)}- ${node.key}: ${node.oldValue}`,
-  [types.UNCHANGED]: (node, level) => `${TAB.repeat(level)}  ${node.key}: ${node.oldValue}`,
-  [types.CHANGED]: (node, level) => `${TAB.repeat(level)}- ${node.key}: ${node.oldValue}\n${TAB.repeat(level)}+ ${node.key}: ${node.newValue}}`,
+  [types.ADDED]: ({ key, newValue, children }, level) => `${TAB.repeat(level)}+ ${key}: ${showValue(newValue, level, children)}`,
+  [types.REMOVED]: ({ key, oldValue, children }, level) => `${TAB.repeat(level)}- ${key}: ${showValue(oldValue, level, children)}`,
+  [types.UNCHANGED]: ({ key, oldValue, children }, level) => `${TAB.repeat(level)}  ${key}: ${oldValue}`,
+  [types.CHANGED]: ({ key, oldValue, newValue, children }, level) => `${TAB.repeat(level)}- ${key}: ${showValue(oldValue, level, children)}\n${TAB.repeat(level)}+ ${key}: ${showValue(newValue, level, children)}`,
   // eslint-disable-next-line no-use-before-define
-  [types.NODE]: (node, level) => `${TAB.repeat(level)}  ${node.key}: ${diff(node.children, level + 1)}`,
+  [types.NODE]: (node, level) => `${TAB.repeat(level)}${node.key}: ${diff(node.children, level + 1)}`,
 };
 
 function diff(data, level) {
--- src/gendiff.js
+++ src/gendiff.js
@@ -9,31 +9,44 @@ const dictionary = [
   },
   {
     check: (file1, file2) => !_.isUndefined(file1) && _.isUndefined(file2),
-    status: (file1) => ({ oldValue: file1, type: types.REMOVED }),
+    status: (file1, file2, takeNext, takeChildren) => ({ oldValue: file1, type: types.REMOVED, children: takeChildren(file1)}),
   },
   {
     check: (file1, file2) => _.isUndefined(file1) && !_.isUndefined(file2),
-    status: (file1, file2) => ({ newValue: file2, type: types.ADDED }),
+    status: (file1, file2, takeNext, takeChildren) => ({ newValue: file2, type: types.ADDED, children: takeChildren(file2) }),
   },
   {
     check: (file1, file2) => !_.isEqual(file1, file2),
-    status: (file1, file2) => ({ oldValue: file1, newValue: file2, type: types.CHANGED }),
+    status: (file1, file2, takeNext, takeChildren) => ({ oldValue: file1, newValue: file2, type: types.CHANGED, children: takeChildren(file2) }),
   },
   {
     check: (file1, file2) => _.isEqual(file1, file2),
-    status: (file1) => ({ oldValue: file1, type: types.UNCHANGED }),
+    status: (file1, file2, takeNext, takeChildren) => ({ oldValue: file1, type: types.UNCHANGED, children: takeChildren(file1) }),
   },
 ];
 
+const getChildren = (data) => {
+  const res = [];
+  if (_.isObject(data)) {
+    const children = Object.entries(data);
+    children.map((child) => {
+      if (_.isObject(child.value)) {
+        getChildren(child.value);
+      } else {
+        res.push(child);
+      }
+    });
+  }
+  return res;
+};
 const genDiff = (file1, file2) => {
   const keys = _.union(Object.keys(file1), Object.keys(file2)).sort();
   return keys.map((key) => {
     const file1Value = file1[key];
     const file2Value = file2[key];
-
     const { status } = dictionary.find(({ check }) => check(file1Value, file2Value));
 
-    return { key, ...status(file1Value, file2Value, genDiff) };
+    return { key, ...status(file1Value, file2Value, genDiff, getChildren) };
   });
 };
 
