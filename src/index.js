d430c554eb460784282c76598f4c281ccbf03fdc
egorlitin <egorulitin@gmail.com>
2022-04-13 13:20:49 +0300

    update enterpoint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import formatter from './formatter/index.js';
 import genDiff from './genDiff.js';
 import getFile from './helpers/getFile.js';
 
-export default (filepath1, filepath2, format) => {
+export default (filepath1, filepath2, format = 'stylish') => {
   const file1 = getFile(filepath1);
   const file2 = getFile(filepath2);
   const diff = genDiff(file1, file2);
