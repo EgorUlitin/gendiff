8e47054819e630688e2df633ecd8fff0a89bfb11
egorlitin <egorulitin@gmail.com>
2022-04-19 18:02:58 +0300

    refactor naming src/index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import formatter from './formatter/index.js';
 import diff from './diff.js';
 import getFile from './helpers/getFile.js';
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const file1 = getFile(filepath1);
   const file2 = getFile(filepath2);
-  const formatForView = formatter(format);
-  return formatForView(diff(file1, file2));
+  const format = formatter(formatName);
+  return format(diff(file1, file2));
 };
 
 export default genDiff;
