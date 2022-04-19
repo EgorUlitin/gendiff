3b19e38dc85ef3e034f7949a65f58b110fa019f5
egorlitin <egorulitin@gmail.com>
2022-04-19 21:54:02 +0300

    refactor getFile and parsers

--- src/helpers/getFile.js
+++ src/helpers/getFile.js
@@ -3,10 +3,11 @@ import * as fs from 'fs';
 import parser from '../parsers.js';
 
 export default (entriesValue) => {
+  const type = path.extname(entriesValue).slice(1);
   const filePath = path.isAbsolute(entriesValue)
     ? entriesValue
     : path.resolve(process.cwd(), entriesValue);
   const file = fs.readFileSync(filePath, 'utf-8');
 
-  return parser(filePath, file);
+  return parser(file, type);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,3 @@
 import yaml from 'js-yaml';
-import path from 'path';
 
-export default (filepath, file) => {
-  const format = path.extname(filepath);
-
-  const parse = format === '.json' ? JSON.parse(file) : yaml.load(file);
-  // let parse;
-  // if (format === '.json') {
-  //   parse = JSON.parse(file);
-  // } else if (format === '.yml' || format === '.yaml') {
-  //   parse = yaml.load(file);
-  // }
-  return parse;
-};
+export default (data, type) => (type === 'json' ? JSON.parse(data) : yaml.load(data));
