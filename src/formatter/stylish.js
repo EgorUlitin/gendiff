782aa0c9caf3a9d596bff072b487e2db301a68f9
egorlitin <egorulitin@gmail.com>
2022-04-27 10:19:52 +0300

    refactor stylish

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -6,28 +6,28 @@ const spacesCount = 4;
 
 const getReplacer = (counter) => TAB.repeat(spacesCount * counter - 2);
 
-const stringify = (node, iter, counter) => {
+const stringify = (node, counter) => {
   if (!_.isObject(node)) {
     return node;
   }
   const entries = Object.entries(node);
-  return `{\n${entries.map(([key, value]) => `${getReplacer(counter)}  ${key}: ${stringify(value, iter, counter + 1)}`).join('\n')}\n${TAB.repeat(spacesCount * counter - 4)}}`;
+  return `{\n${entries.map(([key, value]) => `${getReplacer(counter)}  ${key}: ${stringify(value, counter + 1)}`).join('\n')}\n${TAB.repeat(spacesCount * counter - 4)}}`;
 };
 
 const dictionary = {
-  [types.NODE]: (node, counter, iter) => `${getReplacer(counter)}  ${node.key}: ${iter(node.children, counter + 1)}`,
-  [types.ADD]: (node, counter, iter) => `${getReplacer(counter)}+ ${node.key}: ${stringify(node.value2, iter, counter + 1)}`,
-  [types.REMOVE]: (node, counter, iter) => `${getReplacer(counter)}- ${node.key}: ${stringify(node.value1, iter, counter + 1)}`,
-  [types.CHANGE]: (node, counter, iter) => [
-    `${getReplacer(counter)}- ${node.key}: ${stringify(node.value1, iter, counter + 1)}`,
-    `${getReplacer(counter)}+ ${node.key}: ${stringify(node.value2, iter, counter + 1)}`,
+  [types.NODE]: (node, counter, iter) => `${getReplacer(counter)}  ${node.key}: {\n${node.children.flatMap((child) => iter(child, counter + 1)).join('\n')}\n${TAB.repeat(spacesCount * counter)}}`,
+  [types.ADD]: (node, counter) => `${getReplacer(counter)}+ ${node.key}: ${stringify(node.value2, counter + 1)}`,
+  [types.REMOVE]: (node, counter) => `${getReplacer(counter)}- ${node.key}: ${stringify(node.value1, counter + 1)}`,
+  [types.CHANGE]: (node, counter) => [
+    `${getReplacer(counter)}- ${node.key}: ${stringify(node.value1, counter + 1)}`,
+    `${getReplacer(counter)}+ ${node.key}: ${stringify(node.value2, counter + 1)}`,
   ],
   [types.UNCHANGE]: (node, counter) => `${getReplacer(counter)}  ${node.key}: ${node.value1}`,
-  [types.ROOT]: (node, counter, iter) => `\n${node.children.map((child) => iter(child, counter).join('\n'))}`,
+  [types.ROOT]: (node, counter, iter) => `{\n${node.children.flatMap((child) => iter(child, counter)).join('\n')}\n${TAB.repeat(spacesCount * counter - 4)}}`,
 };
 
 const stylish = (diff) => {
-  const iter = (node, counter) => `{\n${node.flatMap((value) => dictionary[value.type](value, counter, iter)).join('\n')}\n${TAB.repeat(spacesCount * counter - 4)}}`;
-  return iter(diff.children, 1);
+  const iter = (node, counter) => dictionary[node.type](node, counter, iter);
+  return iter(diff, 1);
 };
 export default stylish;
